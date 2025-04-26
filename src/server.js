a1d767505a4bf612e2ff46078639b0c4b906bd38
capsocong <tien262301@gmail.com>
2025-04-26 16:28:49 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+const host = 'localhost'
+const port = 8017
+
+app.get('/',(req,res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`)
+    console.log('Press Ctrl+C to stop server')
+})
