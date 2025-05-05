300fb57c6d0ed950836a57f23fcb2de331bfa8fd
capsocong <tien262301@gmail.com>
2025-05-05 17:54:25 +0700

    code service layer

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,14 +1,14 @@
 
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
+import { boardService } from '~/services/boardService'
 
 const createNew = async (req, res, next) => {
     try {
         // throw new ApiError(StatusCodes.BAD_REQUEST, 'Bad Request')
-        res.status(StatusCodes.CREATED).json({
-            message: 'Create new board successfully',
-            data: req.body
-        })
+        const createdBoard = await boardService.createNew(req.body)
+        res.status(StatusCodes.CREATED).json({createdBoard})
+
     }catch (error) { next(error) }
        
    
