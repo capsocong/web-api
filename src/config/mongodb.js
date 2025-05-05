cdab0b782baa8165ca20a2c69af17794849d9d8e
capsocong <tien262301@gmail.com>
2025-05-05 09:53:07 +0700

    connect_mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,29 @@
 
+const MONGODB_URI = 'mongodb+srv://tientran:2312@cluster0.7jgv1.mongodb.net/webapi?retryWrites=true&w=majority&appName=Cluster0'
+
+
+const DATABASE_NAME = 'webapi'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let WebApiDatabaseInstance = null
+
+const mongoClient = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  // gọi kết nối đến mongodbAtlas với uri đã khai báo bên trong mongoclient
+  await mongoClient.connect()
+
+  WebApiDatabaseInstance = mongoClient.db(DATABASE_NAME)
+}
+// function GET_DB (không async) để export ra webapiDatabaseInstance sau khi đã kết nối thành công tới mongodb để tái sử dụng
+export const GET_DB = () => {
+  if (!WebApiDatabaseInstance) throw new Error('Database not connected')
+  return WebApiDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,28 +1,49 @@
+/* eslint-disable no-console */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
+  const hostname = 'localhost'
+  const port = 8017
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`server running at http://${hostname}:${port}/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`server running at http://${hostname}:${port}`)
-})
+  exitHook(() => {
+    console.log('4 exitapp')
+  })
+}
+(async () => {
+  try {
+    console.log('1.Connecting to MongoDB Atlas')
+    await CONNECT_DB()
+    console.log('2.Connected to MongoDB Atlas')
+    // start server khi kết nối thành công
+    console.log('3.Starting server')
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+})()
+// CONNECT_DB()
+//   .then(() => {
+//     console.log('Connected to MongoDB Atlas')
+//   })
+//   .then( () => { START_SERVER() } )
+
+//   .catch( error => {
+//     console.error('Error connecting to MongoDB Atlas:', error)
+//     process.exit(0)
+//   })
+
