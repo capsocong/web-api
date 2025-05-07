e7b1466f2f10136e5bfa7c340c3c8453cd5ee0d7
capsocong <tien262301@gmail.com>
2025-05-07 15:39:10 +0700

    Api-create-board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -6,7 +6,9 @@ import { boardService } from '~/services/boardService'
 const createNew = async (req, res, next) => {
     try {
         // throw new ApiError(StatusCodes.BAD_REQUEST, 'Bad Request')
+        // dieu huong request den service de xu ly
         const createdBoard = await boardService.createNew(req.body)
+        // có kết quả thì trả về cho client
         res.status(StatusCodes.CREATED).json({createdBoard})
 
     }catch (error) { next(error) }
